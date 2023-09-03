5903491142e1ecd658b8a09a5ea4e10c6818b0bc
Yash <dhikaleyash@gmail.com>
2023-09-03 11:07:22 +0530

    initial commit

---
+++ components/SearchManufacturer.tsx
@@ -0,0 +1,74 @@
+"use client"
+import { useState, Fragment } from 'react';
+import Image from 'next/image';
+import { Combobox, Transition } from '@headlessui/react';
+import { SearchManufacturerProps } from '@/types';
+import React from 'react';
+import { manufacturers } from '@/constants';
+
+const SearchManufacturer = ({ selected, setSelected }: SearchManufacturerProps) => {
+
+  const [query, Setquery] = useState('')
+
+  const filteredManufacturers = query === "" ?
+    manufacturers : manufacturers.filter((item) => (
+      item.toLowerCase()
+        .replace(/\s+/g, "")
+        .includes(query.toLowerCase().replace(/\s+/g, "")
+
+        )))
+
+  return (
+    <div className='search-manufacturer'>
+      <Combobox value={selected} onChange={setSelected}>
+        <div className='relative w-full'>
+          <Combobox.Button className="absolute top-[14px]">
+            <Image
+              src="/car-logo.svg"
+              width={20}
+              height={20}
+              className="ml-4"
+              alt="Car Logo"
+            />
+          </Combobox.Button>
+
+          <Combobox.Input
+            className="search-manufacturer__input"
+            placeholder='volkswagen'
+            displayValue={(manufacturer: string) => manufacturer}
+            onChange={(e) => Setquery(e.target.value)}
+          />
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => Setquery('')}
+          >
+            <Combobox.Options>
+              
+              {filteredManufacturers.map((item)=>(
+              <Combobox.Option
+                key={item}
+                className={({ active }) => `relative search-manufacturer__option
+              ${active ? 'bg-primary-blue text-white' : 'text-gray-900'}
+              `}
+              value={item}
+
+              >
+                {item}
+
+              </Combobox.Option>
+              )
+              )}
+              </Combobox.Options>
+
+
+          </Transition>
+        </div>
+      </Combobox>
+    </div>
+  );
+}
+
+export default SearchManufacturer;
---
+++ constants/index.ts
@@ -0,0 +1,130 @@
+export const manufacturers = [
+    "Acura",
+    "Alfa Romeo",
+    "Aston Martin",
+    "Audi",
+    "Bentley",
+    "BMW",
+    "Buick",
+    "Cadillac",
+    "Chevrolet",
+    "Chrysler",
+    "Citroen",
+    "Dodge",
+    "Ferrari",
+    "Fiat",
+    "Ford",
+    "GMC",
+    "Honda",
+    "Hyundai",
+    "Infiniti",
+    "Jaguar",
+    "Jeep",
+    "Kia",
+    "Lamborghini",
+    "Land Rover",
+    "Lexus",
+    "Lincoln",
+    "Maserati",
+    "Mazda",
+    "McLaren",
+    "Mercedes-Benz",
+    "MINI",
+    "Mitsubishi",
+    "Nissan",
+    "Porsche",
+    "Ram",
+    "Rolls-Royce",
+    "Subaru",
+    "Tesla",
+    "Toyota",
+    "Volkswagen",
+    "Volvo",
+  ];
+  export const yearsOfProduction = [
+    { title: "Year", value: "" },
+    { title: "2015", value: "2015" },
+    { title: "2016", value: "2016" },
+    { title: "2017", value: "2017" },
+    { title: "2018", value: "2018" },
+    { title: "2019", value: "2019" },
+    { title: "2020", value: "2020" },
+    { title: "2021", value: "2021" },
+    { title: "2022", value: "2022" },
+    { title: "2023", value: "2023" },
+  ];
+  export const fuels = [
+    {
+      title: "Fuel",
+      value: "",
+    },
+    {
+      title: "Gas",
+      value: "Gas",
+    },
+    {
+      title: "Electricity",
+      value: "Electricity",
+    },
+  ];
+  
+  export const footerLinks = [
+    {
+    
+      title: "About",
+      links: [
+        { title: "Home", url: "/" },
+        { title: "Catalogue", url: "/" },
+        { title: "Services", url: "/" },
+        { title: "How it works", url: "/" },
+        { title: "Featured", url: "/" },
+        { title: "Partnership", url: "/" },
+        { title: "Bussiness Relation", url: "/" },
+      ],
+    },
+    {
+      title: "Company",
+      links: [
+        { title: "About", url: "/" },
+        { title: "Careers", url: "/" },
+        { title: "Contact", url: "/" },
+        { title: "Events", url: "/" },
+        { title: "Blog", url: "/" },
+        { title: "Podcast", url: "/" },
+        { title: "Invite a friend", url: "/" },
+      ],
+    },
+    {
+    
+      title: "Socials",
+      links: [
+        { title: "Terms", url: "/" },
+        { title: "Privacy", url: "/" },
+        { title: "Discord", url: "/" },
+        { title: "Instagram", url: "/" },
+        { title: "Twitter", url: "/" },
+        { title: "Facebook", url: "/" },
+      ],
+    },
+    {
+      title: "Social media",
+      links: [
+        {
+          title: "Github",
+          url: "https://github.com/adrianhajdin?tab=repositories",
+          icon: "/github.svg",
+        },
+        {
+          title: "Discord",
+          url: "https://discord.gg/KNay9TqYZg",
+          icon: "/discord.svg",
+        },
+        {
+          title: "LinkedIn",
+          url: "https://www.linkedin.com/company/javascriptmastery/",
+          icon: "/linkedin.svg",
+        },
+      ],
+      isSocialMedia: true,
+    },
+];
